dbada1e37008a244b0a7dfa230db4852c7218277
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-09-26 16:36:40 -0400

    changed procfile to remove start.js

--- server/clock.js
+++ server/clock.js
@@ -2,7 +2,7 @@ var CronJob = require('cron').CronJob;
 var bot = require('./bot.js');
 
 new CronJob({
-  cronTime: "1 35,36,37 16 * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
+  cronTime: "1 35,36,37 16 * * *",
   onTick: bot.makeCalls(),
   start: true,
   timeZone: "America/New_York"
