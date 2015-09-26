09cd7d697d22d4425bb0aa195233298857622d82
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-09-26 16:32:20 -0400

    added cron jobs

--- server/clock.js
+++ server/clock.js
@@ -1,9 +1,9 @@
 var CronJob = require('cron').CronJob;
 var bot = require('./bot.js');
 
-// new CronJob({
-//   cronTime: "13 9,11,16,20,22 * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
-//   onTick: bot.makeCalls(),
-//   start: true,
-//   timeZone: "America/New_York"
-// });
+new CronJob({
+  cronTime: "1 35,36,37 16 * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
+  onTick: bot.makeCalls(),
+  start: true,
+  timeZone: "America/New_York"
+});
