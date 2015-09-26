e2295ff0ee6a4de36afdee98df379cdf7d1d44b8
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-09-26 17:57:45 -0400

    changed twiml url

--- server/clock.js
+++ server/clock.js
@@ -3,27 +3,26 @@ var client = require('twilio')('AC35326c4f5ae8dd516f973d837fe4d485',
 var CronJob = require('cron').CronJob;
 var clock = require('./clock.js');
 
-var job = new CronJob('* * * * * *', function() {
-  console.log('You will see this message every second');
-}, null, false, 'America/New_York');
 
-// new CronJob({
-//   cronTime: "1 35 17 * * *",
-//   onTick: function() {
-//     client.makeCall({
-//     to:'+15864199473',
-//     from: '+15862001110',
-//     url: 'http://two-letter-study.appspot.com/static/twiml.xml'//going to be from the twiml
-//   })
-//   .then(function(call) {
-//       console.log('Call success! Call SID: '+call.sid);
-//   }).then(null, console.log)
-// },
-//   onComplete: function(){
-//     console.log('job finished');
-//   },
-//   timeZone: "America/New_York"
-// });
+var job = new CronJob({
+  cronTime: new Date('2015-09-26T18:00:30'),
+  onTick: function() {
+    client.makeCall({
+    to:'+15864199473',
+    from: '+15862001110',
+    url: 'http://two-letter-study.appspot.com/static/twiml.xml'//going to be from the twiml
+  })
+  .then(function(call) {
+      console.log('Call success! Call SID: '+call.sid);
+  }).then(null, console.log)
+},
+  onComplete: function(){
+    console.log('job finished');
+  },
+  timeZone: "America/New_York"
+});
+
+job.start();
 
 
  //  clock.makeCalls = function() {
