ae59a65da65bd1dc0535bd68c78a65c69a2a4f50
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-09-26 18:40:41 -0400

    changed twiml url

--- server/clock.js
+++ server/clock.js
@@ -2,6 +2,8 @@ var client = require('twilio')('AC35326c4f5ae8dd516f973d837fe4d485',
 'c4ac1a1aace3333a3414904c3fa7f918');
 var CronJob = require('cron').CronJob;
 var clock = require('./clock.js');
+var client = require('twilio')('AC35326c4f5ae8dd516f973d837fe4d485',
+'c4ac1a1aace3333a3414904c3fa7f918');
 
 var threeMinutes = 180000;
 var inThreeMinutes = new Date(Date.now()+threeMinutes);
